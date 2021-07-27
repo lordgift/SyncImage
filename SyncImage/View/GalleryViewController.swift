07d15881a11ba44614571c7aad73977a772545d4
LordGift <jarupath.j@cdg.co.th>
2021-07-27 16:49:27 +0700

    select image from picker

--- SyncImage/View/GalleryViewController.swift
+++ SyncImage/View/GalleryViewController.swift
@@ -7,8 +7,8 @@
 
 import UIKit
 
-class GalleryViewController: UIViewController {
-
+class GalleryViewController: UIViewController, UINavigationControllerDelegate {
+    
     @IBOutlet var collectionView: UICollectionView!
     
     lazy var viewModel: GalleryVM = {
@@ -19,7 +19,23 @@ class GalleryViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func handleTapQR(_ sender: UIBarButtonItem) {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let pickerVC = UIImagePickerController()
+            pickerVC.delegate = self
+            pickerVC.sourceType = .photoLibrary;
+            
+            pickerVC.allowsEditing = false
+            self.present(pickerVC, animated: true, completion: nil)
+        }
+    }
+    
 }
 
+extension GalleryViewController: UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let selectedImage =  info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        self.dismiss(animated: true, completion: nil)
+    }
+}
