4bf36a9400b29ab835d6696d8ff89cc8eac05909
LordGift <jarupath.j@cdg.co.th>
2021-07-27 15:29:43 +0700

    initial project

---
+++ SyncImage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SyncImage
+//
+//  Created by LordGift on 27/7/2564 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
