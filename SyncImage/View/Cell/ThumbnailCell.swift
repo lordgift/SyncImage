2a299f01a78a62aabdba4f9bdc7dc14efdffc6b4
LordGift <jarupath.j@gmail.com>
2021-08-01 10:15:20 +0700

    display filetype in cell

--- SyncImage/View/Cell/ThumbnailCell.swift
+++ SyncImage/View/Cell/ThumbnailCell.swift
@@ -11,12 +11,14 @@ class ThumbnailCell: UICollectionViewCell {
     
     @IBOutlet var thumbnailImageView: UIImageView!
     @IBOutlet var syncIconButton: UIButton!
+    @IBOutlet var fileTypeLabel: UILabel!
     
     func setCell(picData: PicData) {
         let imageURL = Util.getDocumentsDirectory().appendingPathComponent(picData.name!)
         let image    = UIImage(contentsOfFile: imageURL.path)
         self.thumbnailImageView.image = image
         self.syncIconButton.isHighlighted =  picData.timestamp != nil
+        self.fileTypeLabel.text = imageURL.pathExtension
     }
 
 }
