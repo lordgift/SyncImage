d4eb66715e18fd3b8b676218d0358037422b5f97
LordGift <jarupath.j@gmail.com>
2021-08-01 20:00:57 +0700

    update cell style

--- SyncImage/View/Gallery/Cell/ThumbnailCell.swift
+++ SyncImage/View/Gallery/Cell/ThumbnailCell.swift
@@ -12,6 +12,7 @@ class ThumbnailCell: UICollectionViewCell {
     @IBOutlet var thumbnailImageView: UIImageView!
     @IBOutlet var syncIconButton: UIButton!
     @IBOutlet var fileTypeLabel: UILabel!
+    @IBOutlet var uploadTimestampLabel: UILabel!
     
     func setCell(picData: PicData) {
         let imageURL = Util.getDocumentsDirectory().appendingPathComponent(picData.name!)
@@ -19,6 +20,31 @@ class ThumbnailCell: UICollectionViewCell {
         self.thumbnailImageView.image = image
         self.syncIconButton.isHighlighted =  picData.timestamp != nil
         self.fileTypeLabel.text = imageURL.pathExtension
+        
+        if picData.timestamp != nil {
+            let date = toDate(from: picData.timestamp)
+            self.uploadTimestampLabel.text = toString(from: date)
+        } else {
+            self.uploadTimestampLabel.text = nil
+        }
+        
     }
-
+    
+    private func toDate(from string: String?) -> Date? {
+        guard let dateString = string else {
+            return nil
+        }
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSSXXXXX"
+        return formatter.date(from: dateString)
+    }
+    
+    private func toString(from date: Date?) -> String? {
+        guard let date = date else {
+            return nil
+        }
+        let formatter = DateFormatter()
+        formatter.dateFormat = "E, d MMM\nHH:mm"
+        return formatter.string(from: date)
+    }
 }
